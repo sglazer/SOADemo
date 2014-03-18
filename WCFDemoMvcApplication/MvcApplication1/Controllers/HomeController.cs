b74e2d2cc3d69eb8020967021be7478c08aa10d1
Stephen Glazer <sjglazer@hotmail.com>
2014-03-18 15:09:10 -0400

    Latest Code Changes

--- WCFDemoMvcApplication/MvcApplication1/Controllers/HomeController.cs
+++ WCFDemoMvcApplication/MvcApplication1/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,12 +11,17 @@ namespace MvcApplication1.Controllers
 {
 	public class HomeController : Controller
 	{
+		string APIHost = ConfigurationManager.AppSettings["APIHost"];
+		string FeaturedItemsServiceVersion = ConfigurationManager.AppSettings["FeaturedItemsServiceVersion"];
+		
+		
 		public ActionResult Index()
 		{
-			ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+			ViewBag.Message = "Feel free to modify this site and play around...";
+			
 			FeaturedItemsModel model = new FeaturedItemsModel();
 			ShawServices.FeaturedItemsClient client = new ShawServices.FeaturedItemsClient();
-			var items = client.GetItem("api.slice.dev.smdg.ca", "latest", "311");
+			var items = client.GetItem(APIHost, FeaturedItemsServiceVersion, "311");
 			model.EditorsPicks = items;
 			return View(model);
 		}
