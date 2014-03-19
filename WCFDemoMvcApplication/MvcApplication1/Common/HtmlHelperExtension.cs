5057b8172eba6502e6dc69ebe51dae081004bc08
Stephen Glazer <sjglazer@hotmail.com>
2014-03-19 09:51:39 -0400

    Adding missing files.

---
+++ WCFDemoMvcApplication/MvcApplication1/Common/HtmlHelperExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+namespace System.Web.Mvc
+{
+	public static class HtmlHelperExtention
+	{
+		public static string FullImageUrl(this HtmlHelper htmlHelper, string url)
+		{
+			if (!url.StartsWith("http"))
+			{
+				return string.Format("{0}{1}", ConfigurationManager.AppSettings["ImageBaseUrl"], url.ToLower());
+			}
+			return string.IsNullOrEmpty(url) ? string.Empty : url;
+		}
+	}
+}
---
+++ WCFDemoMvcApplication/MvcApplication1/Models/HomeModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcApplication1.ShawServices;
+
+
+namespace MvcApplication1.Models
+{
+	public class FeaturedItemsModel
+	{
+		public FeaturedItems EditorsPicks {get;set;}
+	}
+}
